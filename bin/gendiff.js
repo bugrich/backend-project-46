00621c298d0271344794154f9d0534dab85ddca7
bugrich <kyr.deu@gmail.com>
2025-06-21 12:06:11 +0200

    updated gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,8 +4,10 @@ import { Command } from 'commander'
 const program = new Command()
 
 program
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .option('-V, --version', 'output the version number')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .option('-f, --format [type]', 'output format')
 
 program.parse()
